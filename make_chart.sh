e5690f7278fdd57dd844c34769e1559cb8bc2483
Niel M. Bornstein <nbornstein@suse.com>
2020-07-19 14:05:19 -0400

    Added comments

--- make_chart.sh
+++ make_chart.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# get today's data zip file
 curl --output ga_covid_data.zip https://ga-covid19.ondemand.sas.com/docs/ga_covid_data.zip
 
+# extract it
 unzip -o ga_covid_data.zip
 
+# create the chloropleth
 R --save < counties.r
