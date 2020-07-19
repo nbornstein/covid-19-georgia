b8eb84f2eb7dce20737ed553d2b169754a5d1a54
Niel M. Bornstein <nbornstein@suse.com>
2020-07-19 12:13:49 -0400

    Made it a little prettier and corrected the title

--- counties.r
+++ counties.r
@@ -7,15 +7,17 @@ dph_data <- read.table("~/src/covid-19-georgia/countycases.csv", header=TRUE, se
 covid_data <- left_join(dph_data, counties, by = "county_name") 
 
 covid_data %>% 
+  filter(state_name =="Georgia") %>% 
   ggplot(mapping = aes(long, lat, group = group, fill = case_rate)) +
   geom_polygon(color = "#ffffff", size = .25) +
-  scale_fill_gradientn(labels = scales::percent,
-                       guide = guide_colorbar(title.position = "top"), colors = heat.colors) +
+  scale_fill_gradientn(labels = waiver(),
+                       colors = topo.colors(20),
+                       guide = guide_colorbar(title.position = "top")) +
   coord_map(projection = "albers", lat0 = 30, lat1 = 45) +
+  labs(fill = "Positive Tests per 100,000 Residents") +
   theme(legend.title = element_text(),
-        legend.key.width = unit(.5, "in")) +
-  labs(fill = "Positive Tests Per Capita") +
-  theme(legend.position="bottom",
+        legend.key.width = unit(.5, "in"),
+        legend.position="bottom",
         axis.line=element_blank(),
         axis.text=element_blank(),
         axis.ticks=element_blank(),
