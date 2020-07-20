8ff7ed192727743b77b5cf39325321d402e41ffa
Niel M. Bornstein <nbornstein@suse.com>
2020-07-20 15:46:35 -0400

    Trying different colors

--- nytimes-states.r
+++ nytimes-states.r
@@ -1,7 +1,7 @@
 library(dbplyr)
 library(tidyverse)
 library(urbnmapr)
-library(wesanderson) 
+library(viridisLite) 
 
 # load dataset from New York Times
 nyt_data <- read.table("https://github.com/nytimes/covid-19-data/blob/master/us-states.csv?raw=true", 
@@ -21,9 +21,10 @@ covid_data <- left_join(population_data, states, by = c("state" = "state_name"))
 covid_data %>% 
   filter(date =="2020-07-19") %>%
   ggplot(mapping = aes(long, lat, group = group, fill = ( cases / POPESTIMATE2019 ) * 100000)) +
-  geom_polygon(color = "#ffffff", size = .25) +
+  geom_polygon(color = "grey50", size = .25) +
   scale_fill_gradientn(labels = waiver(),
-                       colors = wes_palette("Zissou1", 10, type = "continuous"),
+                       limits = c(1,5000),
+                       colors = viridis(5),
                        guide = guide_colorbar(title.position = "top")) +
   coord_map(projection = "albers", lat0 = 30, lat1 = 45) +
   labs(fill = "Cases per 100,000 residents") +
